ca2fcdb6881358a4b88e934867ce23ad72520d3a
Cheon <a02281127@gmail.com>
2022-04-21 23:05:55 +0900

    2022-04-21 momentum

---
+++ MOMENTUM/app.js
@@ -0,0 +1,13 @@
+// const - 상수, let - 변수
+// 기본적으로 const를 사용하고, 변수의 업데이트가 필요한 경우 let 사용
+// 자바스크립트는 기존 웹 사이트의 보존을 지향하기 때문에 개선 전 기능을 유지하고, 새 기능을 추가
+// ex) var -> const, let
+// 3가지 모두 사용 가능하지만, 개선된 const, let 권장
+const a = 5;
+const b = 2;
+// 자바스크립트는 camelCase를 권장
+const veryLongVariableName = 0;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
