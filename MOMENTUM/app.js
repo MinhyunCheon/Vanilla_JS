a6c35c29c804e08a0c10f60aa23844545eae1d73
Cheon <a02281127@gmail.com>
2022-05-12 23:43:02 +0900

    Events

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,10 +1,15 @@
 // Searching For Elements
 const hellos = document.getElementsByClassName("hello");
 console.log(hellos);
-
 const title = document.getElementsByTagName("h1");
-
 // querySelector 권장, 다수의 태그가 있는 경우 첫번째 요소 반환(모두 선택의 경우 querySelectorAll 사용)
 // css 형식의 검색 가능
 const qTitle = document.querySelector(".hello h1");
 console.log(qTitle);
+
+// Events
+qTitle.style.color = "blue";
+function handleTitleClick() {
+    console.log("clicked!");
+}
+qTitle.addEventListener("click", handleTitleClick);
