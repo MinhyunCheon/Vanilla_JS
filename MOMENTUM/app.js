107fe6b612cf689320a47fcacc1a0f2c3ba45bb9
Cheon <a02281127@gmail.com>
2022-05-16 22:16:23 +0900

    Form Submission

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -5,7 +5,13 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function clickLoginButton() {
-    alert(loginInput.value);
+    // HTML에서 form으로 처리 가능하므로 굳이 JS에서 처리하는 것은 지양
+    const userName = loginInput.value;
+    if(userName === "") {
+        alert("Please write your name.");
+    } else if(userName.length > 15) {
+        alert("Your name is too long.")
+    }
 }
 
 loginButton.addEventListener("click", clickLoginButton)
