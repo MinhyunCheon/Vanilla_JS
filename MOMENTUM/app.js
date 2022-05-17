3bd61df1e6e3da03391e1a8722170ecba34fce39
Cheon <a02281127@gmail.com>
2022-05-17 16:29:22 +0900

    Getting Username

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,17 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");   // 예시용으로 하나의 요소만 있어 간략히 선언
+const displayName = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault(); // submit의 기본 이벤트인 새로고침을 방지하기 위해 선언
-    console.log(event);
-}
-
-function handleLinkClick(event) {
-    event.preventDefault();
-    alert("clicked!");
+    const userName = loginInput.value;
+    
+    // if(userName.length > 0) {   // HTML에서 이미 확인하기 때문에 불필요
+        loginForm.classList.add(HIDDEN_CLASSNAME);
+        displayName.classList.remove(HIDDEN_CLASSNAME);
+        // displayName.innerText = "Hello " + userName;
+        displayName.innerText = `Hello ${userName}`;    // 위 내용 대체
+    // }
+    console.log(userName);
 }
 
 // 브라우저가 onLoginSubmit를 호출할 때, 페이지의 기본 정보를 함께 넘겨준다.
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
