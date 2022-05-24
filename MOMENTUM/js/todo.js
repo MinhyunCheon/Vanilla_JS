fd2cd3610d5bfbc9c25623684790fc26dd138fab
Cheon <a02281127@gmail.com>
2022-05-24 15:40:05 +0900

    Deleting ToDos part One

--- MOMENTUM/js/todo.js
+++ MOMENTUM/js/todo.js
@@ -11,16 +11,16 @@ todos.forEach(addTodoList);
 
 function handleTodoSubmit(event) {
     event.preventDefault();
-    const newTodo = todoInput.value;
+    const newTodoObj = {text: todoInput.value, id: Date.now()};
     todoInput.value = "";
-    todos.push(newTodo);
-    addTodoList(newTodo);
+    todos.push(newTodoObj);
+    addTodoList(newTodoObj);
     setLocalStorageTodos();
 }
 
 function addTodoList(todo) {
     const span = document.createElement("span");
-    span.innerText = todo;
+    span.innerText = todo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", (event) => {
@@ -28,6 +28,7 @@ function addTodoList(todo) {
     });
     
     const li = document.createElement("li");
+    li.id = todo.id;
     li.appendChild(span);
     li.appendChild(button);
 
