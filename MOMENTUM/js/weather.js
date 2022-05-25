3e658b146ba2ea467b5a5c9b4839ed7393a7b2f7
Cheon <a02281127@gmail.com>
2022-05-25 14:58:25 +0900

    weather API

--- MOMENTUM/js/weather.js
+++ MOMENTUM/js/weather.js
@@ -1,6 +1,15 @@
+const API_KEY = "e5cb480da08352301c7820e4736de48f";
+
 navigator.geolocation.getCurrentPosition(p => {
     const lat = p.coords.latitude;
-    const lng = p.coords.longitude;
+    const lon = p.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
 
-    console.log("You live in", lat, lng);
+    fetch(url).then(response => response.json()
+        .then(data => {
+            const weather = document.querySelector("div#weather span:first-child");
+            const city = document.querySelector("div#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+        }));
 }, () => alert("Can't find you. No weather for you."));
